65d029c8c18279f05a93b4b93befe139730cc42c
Prerit Mathur <mathur.prerit@gmail.com>
2020-01-04 10:38:12 +0530

    all functions working

--- trello.js
+++ trello.js
@@ -1,5 +1,4 @@
 
-
 let url;
 
 const allCards = document.getElementById("my-board");
@@ -48,7 +47,7 @@ function update() {
     })
     .then(data => {
       getboardcards(data);
-    })
+    });
 }
 
 function getboardcards(returnedData) {
@@ -60,6 +59,7 @@ function getboardcards(returnedData) {
     //   console.log(data)
     headCards(data);
   }
+
   const addHeadCardButton = document.createElement("button");
   addHeadCardButton.id = "items-list";
   addHeadCardButton.appendChild(document.createTextNode("+ Add New List"));
@@ -72,14 +72,17 @@ function headCards(items) {
   const allCards = document.getElementById("my-board");
   const addHeadCardItem = document.createElement("div");
   addHeadCardItem.className = "head-card";
-  addHeadCardItem.appendChild(document.createTextNode(items.name));
-  // addHeadCardItem.setAttribute('position',headindex)
+  const addHeadCardItemText = document.createElement("div");
+  addHeadCardItemText.appendChild(document.createTextNode(items.name));
+  addHeadCardItemText.className = "head-card-text";
+  addHeadCardItem.appendChild(addHeadCardItemText);
   addHeadCardItem.setAttribute("id", items.id);
   // index=0;
   for (let i = 0; i < items.cards.length; i++) {
     // (localData[headindex].items).push({id:items.cards[i].id,text:items.cards[i].name,position:i})
     const addCardItem = document.createElement("div");
     const text = document.createElement("div");
+    // const btnContainer=document.createElement('div')
     const editButton = document.createElement("button");
     const deleteButton = document.createElement("button");
 
@@ -92,13 +95,14 @@ function headCards(items) {
     deleteButton.className = "delete-card";
     deleteButton.innerText = "\u2716";
     // console.log(items.cards[i])
-    text.setAttribute("onclick", "popup()");
+    text.addEventListener("click", popup);
     editButton.addEventListener("click", putForm);
     deleteButton.addEventListener("click", deleteCard);
 
     addCardItem.appendChild(text);
     addCardItem.appendChild(editButton);
     addCardItem.appendChild(deleteButton);
+    // addCardItem.appendChild(btnContainer)
     addHeadCardItem.appendChild(addCardItem);
   }
   const addCardButton = document.createElement("button");
@@ -113,6 +117,8 @@ function headCards(items) {
 // console.log(localData)
 
 function putForm(obj) {
+  let hide = obj.target;
+  hide.style.display = "none";
   const editFormLayout = obj.target.parentNode;
   const editTheForm = document.createElement("form");
   const editButton = document.getElementById("add-card");
@@ -165,14 +171,17 @@ function deleteCard(obj) {
 }
 
 function addForm(obj) {
+  let hide = obj.target;
+  hide.style.display = "none";
   // console.log(obj.target.parentNode)
   const addFormLayout = obj.target.parentNode;
   const addTheForm = document.createElement("form");
   const addButton = document.getElementById("add-card");
-  // addButton.appendChild(document.createTextNode('Add'));
+  addButton.appendChild(document.createTextNode("Add"));
   addButton.style.display = "none";
   const inputText = document.createElement("input");
   const inputButton = document.createElement("button");
+  inputButton.innerText = "Add";
   inputText.type = "text";
   inputText.placeholder = "Enter Card Name";
   inputText.id = "insert-item";
@@ -203,6 +212,8 @@ function addCard(obj) {
 }
 
 function addListForm(obj) {
+  let hide = obj.target;
+  hide.style.display = "none";
   const addListFormLayout = obj.target.parentNode;
   const addListTheForm = document.createElement("form");
   // editButton.style.display= "none";
@@ -237,88 +248,131 @@ function popup() {
   // console.log(event)
   const checklist = document.getElementById("popup");
   // console.log(checklist)
-// const checklistItems = document.getElementById("popup-content");
+  // const checklistItems = document.getElementById("popup-content");
+  const dataArray = [];
   parent = event.target.parentNode.id;
   checklist.style.display = "block";
   let CID = event.target.parentNode.id;
   // console.log(CID);
-  url = `https://api.trello.com/1/cards/${CID}/checklists?&checkItem_fields=all&key=${API_KEY}&token=${TOKEN}`;
+  url = [
+    `https://api.trello.com/1/cards/${CID}?key=${API_KEY}&token=${TOKEN}`,
+    `https://api.trello.com/1/cards/${CID}/checklists?checkItems=all&checkItem_fields=all&filter=all&fields=all&key=${API_KEY}&token=${TOKEN}`
+  ];
 
-  return fetch(url, {
-    method: "GET"
-  })
-    .then(res => {
-      if (res.ok) {
-        return res.json();
-      }
-    })
-    .then(data => {
-      refreshCardDOM(data);
-    });
+  return Promise.all(
+    url.map(url =>
+      fetch(url, {
+        method: "GET"
+      })
+        .then(res => {
+          if (res.ok) {
+            return res.json();
+          }
+        })
+        .then(data => {
+          dataArray.push(data);
+        })
+    )
+  ).then(() => {
+    refreshCardDOM(dataArray);
+  });
 }
 
 function refreshCardDOM(data) {
+  const dataDes = data[0];
+  const dataChecklist = data[1];
+
+  console.log(dataDes);
+  // console.log('dsfghj',dataChecklist)
+
   const tabCheckBoard = document.getElementById("popup-content");
   while (tabCheckBoard.hasChildNodes()) {
-      tabCheckBoard.removeChild(tabCheckBoard.firstChild);
+    tabCheckBoard.removeChild(tabCheckBoard.firstChild);
   }
-  const checklistItems = document.createElement('div');
-  // console.log(checklistItems);
-  checklistItems.setAttribute("id", data[0].id);
-
+  const cardHeader = document.createElement("div");
+  cardHeader.className = "card-header";
+  cardHeader.appendChild(document.createTextNode(dataDes.name));
   const closebutton = document.createElement("button");
   closebutton.innerText = "x";
   closebutton.className = "close";
   closebutton.addEventListener("click", close);
-  checklistItems.appendChild(closebutton);
+  tabCheckBoard.appendChild(cardHeader);
+  tabCheckBoard.appendChild(closebutton);
 
-  for (let i = 0; i < data[0].checkItems.length; i++) {
-      let status = data[0].checkItems[i].state;
-      let checklistData = data[0].checkItems[i].name;
-      console.log(data[0].checkItems[i].id);
+  const desCard = document.createElement("div");
+  const desCardHead = document.createElement("div");
+  desCardHead.className = "des-card-head";
+  desCardHead.appendChild(document.createTextNode("Description"));
+  const desCardContent = document.createElement("div");
+  desCardContent.className = "des-card-content";
+  desCardContent.appendChild(document.createTextNode(dataDes.desc));
+  desCard.appendChild(desCardHead);
+  desCard.appendChild(desCardContent);
+  tabCheckBoard.appendChild(desCard);
 
-    const addListItem = document.createElement("div");
-    const checkBox = document.createElement("input");
-    const text = document.createElement("div");
-    const editButton = document.createElement("button");
-    const delButton = document.createElement("button");
+  for (dataCheck of dataChecklist) {
+    const checklistItems = document.createElement("div");
+    checklistItems.innerText = dataCheck.name;
+    checklistItems.setAttribute("id", dataCheck.id);
 
-    addListItem.className = "items-checklist";
-    addListItem.setAttribute("id", data[0].checkItems[i].id);
-    checkBox.className = "item-checkbox";
-    checkBox.type = "checkbox";
-    if (status === "incompelete") {
-      checkBox.checked = 0;
-    } else if (status === "complete") {
-      checkBox.checked = 1;
+    for (let j = 0; j < dataCheck.checkItems.length; j++) {
+      let status = dataCheck.checkItems[j].state;
+      let checklistData = dataCheck.checkItems[j].name;
+      // console.log(dataCheck[0].checkItems[i].id);
+
+      const addListItem = document.createElement("div");
+      const checkBox = document.createElement("input");
+      const text = document.createElement("div");
+      const editButton = document.createElement("button");
+      const delButton = document.createElement("button");
+
+      addListItem.className = "items-checklist";
+      addListItem.setAttribute("id", dataCheck.checkItems[j].id);
+      checkBox.className = "item-checkbox";
+      // checkBox.id='the-checkbox'
+      checkBox.type = "checkbox";
+      if (status === "incompelete") {
+        checkBox.checked = 0;
+      } else if (status === "complete") {
+        checkBox.checked = 1;
+      }
+
+      text.appendChild(document.createTextNode(checklistData));
+      editButton.className = "edit-card";
+      editButton.innerText = "\u270D";
+      delButton.className = "delete-item";
+      delButton.innerText = "\u2716";
+      delButton.addEventListener("click", delItem);
+      editButton.addEventListener("click", editItemForm);
+      checkBox.addEventListener("click", strikeItem);
+      if (status === "complete") {
+        text.style.textDecoration = "line-through";
+      } else if (status === "incomplete") {
+        text.style.textDecoration = "none";
+      }
+
+      addListItem.appendChild(checkBox);
+      addListItem.appendChild(text);
+      addListItem.appendChild(editButton);
+      addListItem.appendChild(delButton);
+      checklistItems.appendChild(addListItem);
     }
 
-    text.appendChild(document.createTextNode(checklistData));
-    editButton.className = "edit-card";
-    editButton.innerText = "\u270D";
-    delButton.className = "delete-item";
-    delButton.innerText = "\u2716";
-    delButton.addEventListener("click", delItem);
-    editButton.addEventListener("click", editItemForm);
-    if (status === "complete") {
-      text.style.textDecoration = "line-through";
-    } else if (status === "incomplete") {
-      text.style.textDecoration = "none";
-    }
-
-    addListItem.appendChild(checkBox);
-    addListItem.appendChild(text);
-    addListItem.appendChild(editButton);
-    addListItem.appendChild(delButton);
-    checklistItems.appendChild(addListItem);
+    const addChecklistItem = document.createElement("button");
+    addChecklistItem.className = "items-checklist";
+    addChecklistItem.innerText = "+ Add new checklist item";
+    checklistItems.appendChild(addChecklistItem);
+    addChecklistItem.addEventListener("click", addItemForm);
+    const deleteWholeCheckList = document.createElement("button");
+    deleteWholeCheckList.innerText = "Delete this checklist";
+    deleteWholeCheckList.addEventListener("click", deleteTheChecklist);
+    checklistItems.append(deleteWholeCheckList);
+    tabCheckBoard.appendChild(checklistItems);
   }
-
-  const addChecklistItem = document.createElement("button");
-  addChecklistItem.className = "items-checklist";
-  addChecklistItem.innerText = "+ Add new checklist item";
-  checklistItems.appendChild(addChecklistItem);
-  tabCheckBoard.appendChild(checklistItems)
-  addChecklistItem.addEventListener("click", addItemForm);
+  const newChecklistButton = document.createElement("button");
+  newChecklistButton.innerText = "+ Add new checklist";
+  newChecklistButton.addEventListener("click", addNewChecklistForm);
+  tabCheckBoard.appendChild(newChecklistButton);
 }
 
 function close() {
@@ -329,7 +383,7 @@ function close() {
   // removeElement.removeChild(checklista);
 }
 
-window.onclick = function (event) {
+window.onclick = function(event) {
   // const checklist = document.getElementById("popup");
   if (event.target == checklist) {
     checklist.style.display = "none";
@@ -357,6 +411,9 @@ function delItem(obj) {
 }
 
 function editItemForm(obj) {
+  obj.preventDefault();
+  let hide = obj.target;
+  hide.style.display = "none";
   const editFormLayout = obj.target.parentNode;
   const editTheForm = document.createElement("form");
   const einputText = document.createElement("input");
@@ -393,6 +450,9 @@ function editItem(obj) {
 }
 
 function addItemForm(obj) {
+  obj.preventDefault();
+  let hide = obj.target;
+  hide.style.display = "none";
   const addItemFormLayout = obj.target.parentNode;
   const addTheItem = document.createElement("form");
   const addItemInputText = document.createElement("input");
@@ -420,9 +480,71 @@ function addItem(obj) {
   //   .then(() => getboardcards());
 }
 
-// function strikeItem(obj) {
-//   parent=obj.target.parentNode
-//   const itemPosition=parent.attributes.position.value;
-//   console.log(parent.firstChild.checked)
-//   localData[itemPosition].status=parent.firstChild.checked;
-// }
+function strikeItem(obj) {
+  // console.log(parent)
+  obj.preventDefault();
+  console.log(obj.target);
+  // console.log(checkBox)
+  const CID = obj.target.parentNode.id;
+  const PCID = parent;
+
+  let state;
+  if (obj.target.checked === false) {
+    state = "incomplete";
+    // text.style.textDecoration = "none";
+  } else if (obj.target.checked === true) {
+    state = "complete";
+    // text.style.textDecoration = "line-through";
+  }
+  url = `https://api.trello.com/1/cards/${PCID}/checkItem/${CID}?state=${state}&key=${API_KEY}&token=${TOKEN}`;
+  return fetch(url, {
+    method: "PUT"
+  }).then(res => {
+    if (res.ok) {
+      return res.json();
+    }
+  });
+}
+
+function addNewChecklistForm(obj) {
+  obj.preventDefault();
+  let hide = obj.target;
+  hide.style.display = "none";
+  const addCheckListFormLayout = obj.target.parentNode;
+  const addTheChecklist = document.createElement("form");
+  const addCheckListInputText = document.createElement("input");
+  addCheckListInputText.type = "text";
+  addCheckListInputText.placeholder = "Add Item";
+  addCheckListInputText.id = "add-checklist";
+  addTheChecklist.appendChild(addCheckListInputText);
+  addCheckListFormLayout.appendChild(addTheChecklist);
+  addTheChecklist.addEventListener("submit", addNewChecklist);
+}
+
+function addNewChecklist(obj) {
+  obj.preventDefault();
+  const CID = parent;
+  const CINAME = document.getElementById("add-checklist").value;
+  // console.log(CINAME)
+  console.log(CID)
+  url = `https://api.trello.com/1/checklists?idCard=${CID}&name=${CINAME}&key=${API_KEY}&token=${TOKEN}`
+  return fetch(url, {
+    method: "POST"
+  }).then(res => {
+    if (res.ok) {
+      return res.json();
+    }
+  });
+}
+
+function deleteTheChecklist(obj) {
+  const CID = obj.target.parentNode.id;
+  url = `https://api.trello.com/1/checklists/${CID}?key=${API_KEY}&token=${TOKEN}`;
+  return fetch(url, {
+    method: "DELETE"
+  }).then(res => {
+    if (res.ok) {
+      return res.json();
+    }
+  });
+}
